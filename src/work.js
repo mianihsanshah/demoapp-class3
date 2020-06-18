e31d300444b99e6056c23096548f5e12287a1608
mianihsanshah <mianihsanshah@gmail.com>
2020-06-18 15:34:24 +0500

    component created in react app

---
+++ src/work.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Work(props){
+    return(
+        <div>
+        <h1><strong><i><u>Assignment#3 ToDo List</u></i> </strong></h1>
+        <hr/>             
+        <h3>1:  Create a React App</h3>
+        <hr/>
+        <h3>2:  Add this Reeact App to GitHub</h3>
+        <hr/>
+        <h3>3:  Create a child component using props and relate it to parent component in App.js</h3>
+        <hr/>
+        <h3>4:  Create GitHub WorkFlow for this app to upload it on Surge</h3>
+        <hr/>
+        <h3>5:  Report surge URL and {props.GitHub} URL to  {props.TutorName} repository as an issue</h3>
+        </div>
+    );
+}
+export default Work;
---
+++ work.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Work(props){
+    return(
+        <div>
+            <h1><strong><i><u>Assignment3 ToDo List</u></i> </strong></h1>
+            <hr></hr>
+            <h3>Create a React App</h3>
+            <hr></hr>
+            <h3>Add this Reeact App to GitHub</h3>
+            <hr></hr>
+            <h3>Create a child component using props and relate it to parent component in App.js</h3>
+            <hr></hr>
+            <h3>Create GitHub WorkFlow for this app to upload it on Surge</h3>
+            <hr></hr>
+            <h3>Report surge URL and GitHub repository URL to Mr. Amir Pinger repository as an issue</h3>
+        </div>
+    );
+}
+export default Work;
